245b5374747ccde2ef2d1c4b4bc1d30432abe826
GenStarter <64079290+GenStarter@users.noreply.github.com>
2020-04-22 20:28:49 +0300

    Create 2_1.cpp

---
+++ Chapter2/2_1.cpp
@@ -0,0 +1,10 @@
+//1. Write a C++ program that displays your name and address (or if you value your privacy,a fictitious name and address).
+
+#include <iostream>
+
+int main(void){
+
+std::cout<<"My name is John Smith"<<std::endl;
+return 0;
+
+}
